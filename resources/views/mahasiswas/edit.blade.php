fbc1f438cda7edc37f6c356ebfd671a9619e532d
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-23 10:51:15 +0700

    feat(view): integrate view to database

---
+++ resources/views/mahasiswas/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('mahasiswas.layout')
+@section('content')
+    <div class="container mt-5">
+        <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+                Edit Mahasiswa
+            </div>
+            <div class="card-body">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+        <form method="post" action="{{ route('mahasiswa.update', $Mahasiswa->nim) }}" id="myForm">
+        @csrf
+        @method('PUT')
+            <div class="form-group">
+                <label for="nim">Nim</label>
+                <input type="text" name="nim" class="formcontrol" id="nim" value="{{ $Mahasiswa->nim }}" ariadescribedby="Nim" >
+            </div>
+            <div class="form-group">
+                <label for="name">Nama</label>
+                <input type="text" name="name" class="formcontrol" id="name" value="{{ $Mahasiswa->name }}" ariadescribedby="name" >
+            </div>
+            <div class="form-group">
+                <label for="kelas">Kelas</label>
+                <input type="kelas" name="kelas" class="formcontrol" id="kelas" value="{{ $Mahasiswa->kelas }}" ariadescribedby="kelas" >
+            </div>
+            <div class="form-group">
+                <label for="jurusan">Jurusan</label>
+                <input type="jurusan" name="jurusan" class="formcontrol" id="jurusan" value="{{ $Mahasiswa->jurusan }}" ariadescribedby="jurusan" >
+            </div>
+            <div class="form-group">
+                <label for="no_handphone">No_Handphone</label>
+                <input type="no_handphone" name="no_handphone" class="formcontrol" id="no_handphone" value="{{ $Mahasiswa->no_handphone }}" ariadescribedby="no_handphone" >
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    </div>
+    </div>
+</div>
+@endsection
