fdea9a2c38727a5a060dffc7f2cc2035e9453fb9
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-23 10:50:21 +0700

    feat(models): change default table laravel

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloq
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
+    protected $table='mahasiswa'; //Eloquent akan membuat model mahasiswa menyimpan record di table mahasiswas
+    public $timestamps=false;
+    protected $primaryKey='nim'; //Mengambil isi DB dengan primaryKey
+
+    protected $fillable = [
+        'nim',
+        'name',
+        'kelas',
+        'jurusan',
+        'no_handphone'
+    ];
 }
